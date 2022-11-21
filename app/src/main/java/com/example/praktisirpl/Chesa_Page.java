9b595b15b5f5ce050782bcb31a16703b5243d816
ChesaSadewa <chesasadewa13@student.uns.ac.id>
2022-11-21 23:52:20 +0700

    Chesa Page

---
+++ app/src/main/java/com/example/praktisirpl/Chesa_Page.java
@@ -0,0 +1,14 @@
+package com.example.praktisirpl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chesa_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chesa_page);
+    }
+}
