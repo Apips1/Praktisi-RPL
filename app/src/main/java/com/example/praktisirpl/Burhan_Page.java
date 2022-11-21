3b7187254e6b35c60e197eac7fdfc98d32408c7f
Afif Nugroho <92508998+Apips1@users.noreply.github.com>
2022-11-22 00:14:58 +0700

    Burhan Page

---
+++ app/src/main/java/com/example/praktisirpl/Burhan_Page.java
@@ -0,0 +1,14 @@
+package com.example.praktisirpl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Burhan_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_burhan_page);
+    }
+}
